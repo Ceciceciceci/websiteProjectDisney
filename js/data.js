e295c4209fc78e60c972ddcd1230c73cde05f397
Ceci Tran <ceci@Cecis-MacBook-Pro.local>
2017-04-23 13:08:22 -0700

    updated graphs and added in functions to add up total sales, and graph charts based on data array

--- js/data.js
+++ js/data.js
@@ -100,10 +100,28 @@ var dataset = [
 /**************************************************************************************************************/
 /**************************************************************************************************************/
 /**************************************************************************************************************/
+//*******************TO-DO
+//set toggle condition for side nav with fa-bars
+//grab fa-bars and also subtract left on col blocks
+//custom if with variables for dropdown options
+    //replace "" "with dropdown values"
+    //need two other conditions
+//add filter table option
 
+// var aRequest = new XMLHttpRequest();
+// aRequest.open('GET', 'dataset.json');
+// aRequest.onload = function(){
+//    console.log(aRequest.responseText);
+//    var aData = aRequest.responseText;
+//    var aData = JSON.parse(aRequest.responseText);
+// };
+// aRequest.send();
+
+//variables
 var salesArray=[];
 var label=[];
-//need drop down get id for year, quarter, and region
+
+//ID elements needed for year, quarter, and region
 var yearDropdown = document.getElementById('yearDrop');
 var quartDropdown = document.getElementById('quarterDrop');
 var regionDropdown = document.getElementById('regionDrop');
@@ -115,12 +133,16 @@ function add(a, b) {
 
 for (i=0; i < dataset.length; i++){
     //custom if with variables for dropdown options
-    // if(dataset[i].region === "South East" && dataset[i].quarter === 1 && dataset[i].year === 2013){
-    if(dataset[i].region === "South West" && dataset[i].year === 2013){
+    //replace "" "with dropdown values"
+    //need two other conditions
+    // if(dataset[i].quarter === 1){}
+    if(dataset[i].year === 2013 && dataset[i].region === "South East"){
+    // if(dataset[i].quarter === 1 && dataset[i].year === 2013 && dataset[i].region === "South East"){
+    // if(dataset[i].region === "South West" && dataset[i].year === 2013){
         salesArray.push(parseInt(dataset[i].sales));
-        label.push(dataset[i].regional_manager);
+        label.push(dataset[i].regional_manager); // create labels needed for bar graph
         console.log(salesArray);
-        var sum = salesArray.reduce(add, 0);
+        var sum = salesArray.reduce(add, 0); //add up all sales in the new array
     }
 }
 
@@ -130,51 +152,42 @@ $(document).ready(function() {
     document.getElementById('total-sales').innerHTML = sum;
 });
 
-var ctx = document.getElementById("myChart")
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: label,
-        datasets: [{
-            label: '# of Votes',
-            data: salesArray,
-            backgroundColor: [
-                '#DFE10D',
-                '#E8620C',
-                '#40B9A4',
-                '#202E4A',
-                '#E6E4E2'
-            ],
-            borderWidth: 0,
-        }],
-    },
-    options: {
-        legend: {
-           display: true,
-           labels: {
-               fontColor: '#2E2E2E',
-               fontFamily: "Open Sans",
-               fontSize: 12,
-               boxWidth: 10,
-           }
-        }, //end of legends
-        scales: {
-            xAxes: [{
-                gridLines: {
-                  display: false
-                },
-                ticks: {
-                  fontFamily: "Open Sans",
-                  fontSize: 12
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true,
-                    fontFamily: "Open Sans",
-                    fontSize: 12
-                }
-            }]
-        } //end of scales
+/**************************************************************************************************************/
+/******************************************* TABLE FUNCTIONS **************************************************/
+
+//Fill up table with the dataset
+function printFullTable(data)
+{
+    var table = document.getElementById('fullDataSetTable'); //grab table
+    for (var i = 0; i < data.length; i++)
+    {
+        var dataset1= data[i];
+        var row = document.createElement('tr'); // create row
+        var properties = ['year', 'quarter', 'region', 'regional_manager', 'sales']; //properties for the row
+
+        for (var j = 0; j < properties.length; j++) // append each one of them
+        {
+            var cell = document.createElement('td'); //names for cell
+            cell.innerHTML = dataset1[properties[j]];
+            row.appendChild(cell); //append value to cell inthat row
+        }
+        table.appendChild(row); //add row
     }
-});
+}
+printFullTable(dataset);
+
+//button print table data
+function printTable()
+{
+   var printTable=document.getElementById("fullDataSetTable");
+   openWin= window.open("");
+   openWin.document.write(printTable.outerHTML);
+   openWin.print();
+   openWin.close();
+}
+//grab button
+$('.print-button').on('click',function(){
+    printTable();
+})
+
+//add filter table option
--- js/graph.js
+++ js/graph.js
@@ -1,19 +1,372 @@
 // for the graphs
+//ctrl+f: chart #
+
+/***********************************************
+****************** chart 1 **********************
+*************************************************/
+var ctx = document.getElementById("myChart")
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: label,
+        datasets: [{
+            label: 'SALES ($)',
+            data: salesArray,
+            backgroundColor: [
+                '#DFE10D',
+                '#E8620C',
+                '#40B9A4',
+                '#202E4A'
+            ],
+            borderWidth: 0,
+        }],
+    },
+    options: {
+        legend: {
+           display: false,
+           labels: {
+               fontColor: '#2E2E2E',
+               fontFamily: "Open Sans",
+               fontSize: 16,
+               boxWidth: 12,
+           }
+        }, //end of legends
+        title: {
+          display: true,
+          text: 'Sales For Quarter 1',
+          fontFamily: "Open Sans",
+          fontSize: 15
+        },
+        scales: {
+            xAxes: [{
+                gridLines: {
+                  display: false
+                },
+                ticks: {
+                  fontFamily: "Open Sans",
+                  fontSize: 12
+                },
+                scaleLabel: {
+                  display: true,
+                  fontFamily: "Open Sans",
+                  fontSize: 12,
+                  labelString: 'Regional Manager'
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true,
+                    fontFamily: "Open Sans",
+                    fontSize: 12
+                },
+                scaleLabel: {
+                  display: true,
+                  fontFamily: "Open Sans",
+                  fontSize: 12,
+                  labelString: 'Sales'
+                }
+            }]
+        } //end of scales
+    }
+});
+
+
+/***********************************************
+****************** chart 2 **********************
+*************************************************/
 
   var ctx2 = document.getElementById("myChart2");
   var myChart = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: label,
+        datasets: [{
+            label: 'SALES ($)',
+            data: [30300,50000,41050,60700],
+            backgroundColor: [
+                '#DFE10D',
+                '#E8620C',
+                '#40B9A4',
+                '#202E4A'
+            ],
+            borderWidth: 0,
+        }],
+    },
+    options: {
+        legend: {
+           display: false,
+           labels: {
+               fontColor: '#2E2E2E',
+               fontFamily: "Open Sans",
+               fontSize: 16,
+               boxWidth: 12,
+           }
+        }, //end of legends
+        title: {
+          display: true,
+          text: 'Sales For Quarter 2',
+          fontFamily: "Open Sans",
+          fontSize: 15
+        },
+        scales: {
+            xAxes: [{
+                gridLines: {
+                  display: false
+                },
+                ticks: {
+                  fontFamily: "Open Sans",
+                  fontSize: 12
+                },
+                scaleLabel: {
+                  display: true,
+                  fontFamily: "Open Sans",
+                  fontSize: 12,
+                  labelString: 'Regional Manager'
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true,
+                    fontFamily: "Open Sans",
+                    fontSize: 12
+                },
+                scaleLabel: {
+                  display: true,
+                  fontFamily: "Open Sans",
+                  fontSize: 12,
+                  labelString: 'Sales'
+                }
+            }]
+        } //end of scales
+    }
+});
+
+
+  /***********************************************
+  ****************** chart 3 **********************
+  *************************************************/
+  var ctx3 = document.getElementById("myChart3");
+  var myChart = new Chart(ctx3, {
+    type: 'bar',
+    data: {
+        labels: label,
+        datasets: [{
+            label: 'SALES ($)',
+            data: [7000,4000,3000,5000],
+            backgroundColor: [
+                '#DFE10D',
+                '#E8620C',
+                '#40B9A4',
+                '#202E4A'
+            ],
+            borderWidth: 0,
+        }],
+    },
+    options: {
+        legend: {
+           display: false,
+           labels: {
+               fontColor: '#2E2E2E',
+               fontFamily: "Open Sans",
+               fontSize: 16,
+               boxWidth: 12,
+           }
+        }, //end of legends
+        title: {
+          display: true,
+          text: 'Sales For Quarter 3',
+          fontFamily: "Open Sans",
+          fontSize: 15
+        },
+        scales: {
+            xAxes: [{
+                gridLines: {
+                  display: false
+                },
+                ticks: {
+                  fontFamily: "Open Sans",
+                  fontSize: 12
+                },
+                scaleLabel: {
+                  display: true,
+                  fontFamily: "Open Sans",
+                  fontSize: 12,
+                  labelString: 'Regional Manager'
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true,
+                    fontFamily: "Open Sans",
+                    fontSize: 12
+                },
+                scaleLabel: {
+                  display: true,
+                  fontFamily: "Open Sans",
+                  fontSize: 12,
+                  labelString: 'Sales'
+                }
+            }]
+        } //end of scales
+    }
+});
+
+
+  /***********************************************
+  ****************** chart 4 **********************
+  *************************************************/
+  var ctx4 = document.getElementById("myChart4");
+  var myChart = new Chart(ctx4, {
+    type: 'bar',
+    data: {
+        labels: label,
+        datasets: [{
+            label: 'SALES ($)',
+            data: [4000,6000,8000,1000,5000],
+            backgroundColor: [
+                '#DFE10D',
+                '#E8620C',
+                '#40B9A4',
+                '#202E4A'
+            ],
+            borderWidth: 0,
+        }],
+    },
+    options: {
+        legend: {
+           display: false,
+           labels: {
+               fontColor: '#2E2E2E',
+               fontFamily: "Open Sans",
+               fontSize: 16,
+               boxWidth: 12,
+           }
+        }, //end of legends
+        title: {
+          display: true,
+          text: 'Sales For Quarter 4',
+          fontFamily: "Open Sans",
+          fontSize: 15
+        },
+        scales: {
+            xAxes: [{
+                gridLines: {
+                  display: false
+                },
+                ticks: {
+                  fontFamily: "Open Sans",
+                  fontSize: 12
+                },
+                scaleLabel: {
+                  display: true,
+                  fontFamily: "Open Sans",
+                  fontSize: 12,
+                  labelString: 'Regional Manager'
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true,
+                    fontFamily: "Open Sans",
+                    fontSize: 12
+                },
+                scaleLabel: {
+                  display: true,
+                  fontFamily: "Open Sans",
+                  fontSize: 12,
+                  labelString: 'Sales'
+                }
+            }]
+        } //end of scales
+    }
+});
+
+
+  /***********************************************
+  ****************** chart 5 **********************
+  *************************************************/
+  var ctx5 = document.getElementById("myChart5");
+  var myChart = new Chart(ctx5, {
+    type: 'bar',
+    data: {
+        labels: label,
+        datasets: [{
+            label: 'SALES ($)',
+            data: salesArray,
+            backgroundColor: [
+                '#DFE10D',
+                '#E8620C',
+                '#40B9A4',
+                '#202E4A'
+            ],
+            borderWidth: 0,
+        }],
+    },
+    options: {
+        legend: {
+           display: false,
+           labels: {
+               fontColor: '#2E2E2E',
+               fontFamily: "Open Sans",
+               fontSize: 16,
+               boxWidth: 12,
+           }
+        }, //end of legends
+        title: {
+          display: true,
+          text: 'Sales For Quarter 1',
+          fontFamily: "Open Sans",
+          fontSize: 15
+        },
+        scales: {
+            xAxes: [{
+                gridLines: {
+                  display: false
+                },
+                ticks: {
+                  fontFamily: "Open Sans",
+                  fontSize: 12
+                },
+                scaleLabel: {
+                  display: true,
+                  fontFamily: "Open Sans",
+                  fontSize: 12,
+                  labelString: 'Regional Manager'
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true,
+                    fontFamily: "Open Sans",
+                    fontSize: 12
+                },
+                scaleLabel: {
+                  display: true,
+                  fontFamily: "Open Sans",
+                  fontSize: 12,
+                  labelString: 'Sales'
+                }
+            }]
+        } //end of scales
+    }
+});
+
+  /***********************************************
+  ****************** chart 6 **********************
+  *************************************************/
+  var ctx6 = document.getElementById("myChart6");
+  var myChart = new Chart(ctx6, {
       type: 'bar',
       data: {
-          labels: ["LEO", "DONA", "PAUL", "MICHAEL", "RAPHAEL"],
+          labels: ["George", "Michaelangelo", "Paul", "Raphael"],
           datasets: [{
-              label: 'undefined',
-              data: [10, 20, 15, 5, 25],
+              label: 'SALES ($)',
+              data: [10000, 20500, 15200, 35400],
               backgroundColor: [
                   '#DFE10D',
                   '#E8620C',
                   '#40B9A4',
-                  '#202E4A',
-                  '#E6E4E2'
+                  '#202E4A'
               ],
               borderWidth: 0,
           }],
@@ -24,8 +377,109 @@
            labels: {
                fontColor: '#2E2E2E',
                fontFamily: "Open Sans",
-               fontSize: 12,
-               boxWidth: 10,
+               fontSize: 16,
+               boxWidth: 12,
+           }
+        }, //end of legends
+          scales: {
+              xAxes: [{
+                  gridLines: {
+                    display: false
+                  },
+                  ticks: {
+                    fontFamily: "Open Sans",
+                    fontSize: 12
+                  }
+              }],
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true,
+                      fontFamily: "Open Sans",
+                      fontSize: 12
+                  }
+              }]
+          }
+      }
+  });
+  /***********************************************
+  ****************** chart 7 **********************
+  *************************************************/
+  var ctx7 = document.getElementById("myChart7");
+  var myChart = new Chart(ctx7, {
+      type: 'bar',
+      data: {
+          labels: ["George", "Michaelangelo", "Paul", "Raphael"],
+          datasets: [{
+              label: 'SALES ($)',
+              data: [10000, 20500, 15200, 35400],
+              backgroundColor: [
+                  '#DFE10D',
+                  '#E8620C',
+                  '#40B9A4',
+                  '#202E4A'
+              ],
+              borderWidth: 0,
+          }],
+      },
+      options: {
+        legend: {
+           display: true,
+           labels: {
+               fontColor: '#2E2E2E',
+               fontFamily: "Open Sans",
+               fontSize: 16,
+               boxWidth: 12,
+           }
+        }, //end of legends
+          scales: {
+              xAxes: [{
+                  gridLines: {
+                    display: false
+                  },
+                  ticks: {
+                    fontFamily: "Open Sans",
+                    fontSize: 12
+                  }
+              }],
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true,
+                      fontFamily: "Open Sans",
+                      fontSize: 12
+                  }
+              }]
+          }
+      }
+  });
+
+  /***********************************************
+  ****************** chart 8 **********************
+  *************************************************/
+  var ctx8 = document.getElementById("myChart8");
+  var myChart = new Chart(ctx8, {
+      type: 'bar',
+      data: {
+          labels: ["George", "Michaelangelo", "Paul", "Raphael"],
+          datasets: [{
+              label: 'SALES ($)',
+              data: [10000, 20500, 15200, 35400],
+              backgroundColor: [
+                  '#DFE10D',
+                  '#E8620C',
+                  '#40B9A4',
+                  '#202E4A'
+              ],
+              borderWidth: 0,
+          }],
+      },
+      options: {
+        legend: {
+           display: true,
+           labels: {
+               fontColor: '#2E2E2E',
+               fontFamily: "Open Sans",
+               fontSize: 16,
+               boxWidth: 12,
            }
         }, //end of legends
           scales: {
